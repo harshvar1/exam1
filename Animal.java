1dbcc79be19df21d1b37dd0d2fa291d87616a901
HARSH VARDHAN <harshvardhan15977@gmail.com>
2025-06-18 11:11:56 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Animal
+{
+    String name;
+    public void eat(){
+        System.out.println("I can eat");
+    }
+
+
+}
+
+class dog extends Animal {
+    public void display() {
+        System.out.println("My name is "+  name);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        dog labrador=new dog();
+
+        labrador.name ="rohu";
+        labrador.display();
+        labrador.eat();
+        
+    }
+}
